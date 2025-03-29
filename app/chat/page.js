06ef227ddae034e1060d6e4cea6b9519820e2b18
Lienkulet <clocicovalexandru@gmail.com>
2025-03-29 14:16:46 +0200

    fixes

--- app/chat/page.js
+++ app/chat/page.js
@@ -4,14 +4,14 @@ import ChatSection from "@/components/Chat";
 import ReportSection from "@/components/Report";
 import { useState } from "react";
 
-const page = () => {
+const Page = () => {
     const [summary, setSummary] = useState(''); 
     return (
         <section className="app-container">
           <ReportSection summary={summary} setSummary={setSummary} />
           <ChatSection summary={summary} />
         </section>
-      );
+    );
 }
 
-export default page
+export default Page;
